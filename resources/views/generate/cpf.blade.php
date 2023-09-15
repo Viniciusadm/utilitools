c45e3776d47e631839c5dc69506fa920e298b2c8
viniciusam <viniciusam.22@gmail.com>
2023-09-14 22:15:06 -0300

    Tela de gerar CPF melhor

--- resources/views/generate/cpf.blade.php
+++ resources/views/generate/cpf.blade.php
@@ -4,89 +4,96 @@
 
 @section('content')
     <main class="p-4 bg-a-light dark:bg-a-dark rounded-lg mt-4 w-screen sm:w-content">
-        <h1 class="text-3xl font-semibold">
+        <h1 class="text-2xl font-semibold mb-2 sm:mb-3">
             Gerador de CPF
         </h1>
 
-        <div class="mt-6">
-            <div class="mb-3">
-                <p>
-                    Pontuação?
-                </p>
-                <label class="inline-flex items-center mt-3">
-                    <input type="radio" class="form-radio" name="punctuation" value="1" checked>
-                    <span class="ml-2">Sim</span>
-                </label>
-                <label class="inline-flex items-center mt-3 ml-6">
-                    <input type="radio" class="form-radio" name="punctuation" value="0">
-                    <span class="ml-2">Não</span>
-                </label>
-            </div>
-            <div>
-                <label for="uf" class="block mb-2">
-                    Estado
-                </label>
-                <select name="uf" id="uf" class="block bg-white dark:bg-b-dark p-2 border border-gray-300 dark:border-gray-700 rounded-md shadow-sm">
-                    <option value="">Qualquer</option>
-                    <option value="AC">Acre</option>
-                    <option value="AL">Alagoas</option>
-                    <option value="AM">Amazonas</option>
-                    <option value="AP">Amapá</option>
-                    <option value="BA">Bahia</option>
-                    <option value="CE">Ceará</option>
-                    <option value="DF">Distrito Federal</option>
-                    <option value="ES">Espirito Santo</option>
-                    <option value="GO">Goiás</option>
-                    <option value="MA">Maranhão</option>
-                    <option value="MG">Minas Gerais</option>
-                    <option value="MS">Mato Grosso do Sul</option>
-                    <option value="MT">Mato Grosso</option>
-                    <option value="PA">Pará</option>
-                    <option value="PB">Paraíba</option>
-                    <option value="PE">Pernambuco</option>
-                    <option value="PI">Piauí</option>
-                    <option value="PR">Paraná</option>
-                    <option value="RJ">Rio de Janeiro</option>
-                    <option value="RN">Rio Grande do Norte</option>
-                    <option value="RO">Rondônia</option>
-                    <option value="RR">Roraima</option>
-                    <option value="RS">Rio Grande do Sul</option>
-                    <option value="SC">Santa Catarina</option>
-                    <option value="SE">Sergipe</option>
-                    <option value="SP">São Paulo</option>
-                    <option value="TO">Tocantins</option>
-                </select>
+        <p class="mb-4 sm:mb-6">
+            Gerador de CPFs válidos aleatórios. Você pode escolher se quer pontuação ou não e o estado.
+            Basta clicar em gerar e copiar o CPF gerado.
+        </p>
+
+        <div class="mb-2 sm:mb-3">
+            <p class="mb-2 sm:mb-3">
+                Pontuação?
+            </p>
+            <div class="flex gap-3">
+                <div class="flex items-center">
+                    <input checked id="yes" type="radio" value="1" name="punctuation" class="w-4 h-4 text-blue-600 bg-gray-100">
+                    <label for="yes" class="ml-2 text-sm font-medium">Sim</label>
+                </div>
+                <div class="flex items-center">
+                    <input id="no" type="radio" value="2" name="punctuation" class="w-4 h-4 text-blue-600 bg-gray-100">
+                    <label for="no" class="ml-2 text-sm font-medium">Não</label>
+                </div>
             </div>
         </div>
 
-        <div class="mt-6">
-            <div class="mb-3">
-                <p>
-                    CPF gerado
-                </p>
-                <div class="flex items-center my-3">
-                    <p id="cpf" class="text-2xl font-semibold w-[212px]">
-                        000.000.000-00
-                    </p>
-                    <button id="generate" class="text-white rounded-md ml-3 px-4 py-2 bg-p-light dark:bg-p-dark hover:bg-p-light-light dark:hover:bg-p-dark-light">
-                        Gerar
-                    </button>
-                </div>
-                <div class="flex gap-2">
-                    <button id="copy" class="text-white rounded-md px-4 py-2 bg-p-light dark:bg-s-dark hover:bg-s-light-light dark:hover:bg-s-dark-light">
-                        Copiar
-                    </button>
-                    <a
-                        href="{{ route('validate.cpf') }}"
-                        class="text-white rounded-md px-4 py-2 bg-s-light dark:bg-s-dark hover:bg-s-light-light dark:hover:bg-s-dark-light"
-                        id="validate"
-                    >
-                        Validar CPF
-                    </a>
-                </div>
-            </div>
+        <label for="uf" class="block mb-2 sm:mb-3">
+            Estado
+        </label>
+        <select
+            id="uf"
+            class="border rounded-lg block p-2.5 bg-white dark:bg-a-dark mb-2 sm:mb-3 w-full sm:w-40"
+        >
+            <option value="">Qualquer</option>
+            <option value="AC">Acre</option>
+            <option value="AL">Alagoas</option>
+            <option value="AM">Amazonas</option>
+            <option value="AP">Amapá</option>
+            <option value="BA">Bahia</option>
+            <option value="CE">Ceará</option>
+            <option value="DF">Distrito Federal</option>
+            <option value="ES">Espirito Santo</option>
+            <option value="GO">Goiás</option>
+            <option value="MA">Maranhão</option>
+            <option value="MG">Minas Gerais</option>
+            <option value="MS">Mato Grosso do Sul</option>
+            <option value="MT">Mato Grosso</option>
+            <option value="PA">Pará</option>
+            <option value="PB">Paraíba</option>
+            <option value="PE">Pernambuco</option>
+            <option value="PI">Piauí</option>
+            <option value="PR">Paraná</option>
+            <option value="RJ">Rio de Janeiro</option>
+            <option value="RN">Rio Grande do Norte</option>
+            <option value="RO">Rondônia</option>
+            <option value="RR">Roraima</option>
+            <option value="RS">Rio Grande do Sul</option>
+            <option value="SC">Santa Catarina</option>
+            <option value="SE">Sergipe</option>
+            <option value="SP">São Paulo</option>
+            <option value="TO">Tocantins</option>
+        </select>
+
+        <button
+            id="generate"
+            class="text-white rounded-md px-4 py-2 bg-p-light dark:bg-p-dark hover:bg-p-light-light dark:hover:bg-p-dark-light mb-4 sm:mb-6 block text-center w-full sm:w-40"
+        >
+            Gerar
+        </button>
+
+        <p>
+            CPF gerado
+        </p>
+
+        <div class="flex gap-2 items-center mb-2 sm:mb-3">
+            <p id="cpf" class="text-2xl font-semibold my-3 w-[212px]">
+                000.000.000-00
+            </p>
+            <button id="copy" class="text-white bg-transparent rounded-md h-10 px-4 border border-white">
+                <i class="bi-clipboard"></i>
+            </button>
         </div>
-    </main>
+
+        <a
+            href="{{ route('validate.cpf') }}"
+            class="text-white rounded-md px-4 py-2 bg-s-light dark:bg-s-dark hover:bg-s-light-light dark:hover:bg-s-dark-light mb-4 sm:mb-6 block text-center w-full sm:w-40"
+            id="validate"
+        >
+            Validar CPF
+        </a>
+     </main>
 @endsection
 
 @vite('resources/js/generate/cpf.js')
