9ebbb70087543f4184a09eebe2e2a6e950627f38
viniciusam <viniciusam.22@gmail.com>
2023-09-16 18:06:18 -0300

    Home temporária

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,5 +12,107 @@
 @endsection
 
 @section('content')
+    <main class="p-4 bg-a-light dark:bg-a-dark rounded-lg mt-4 w-screen sm:w-content">
+        <h1 class="text-2xl font-semibold mb-2 sm:mb-3">
+            Principais ferramentas
+        </h1>
 
+        <div class="grid grid-cols-3 gap-3">
+            <a
+                href="{{ route('generate.cpf') }}"
+                class="bg-p-light dark:bg-p-dark rounded-lg p-4 text-white hover:bg-p-light-light dark:hover:bg-p-dark-light"
+                aria-label="Gerador de CPF"
+            >
+                <h2 class="text-xl font-semibold mb-2">
+                    Gerador de CPF
+                </h2>
+                <p class="mb-2">
+                    Gere um ou mais CPFs válidos.
+                </p>
+                <p class="text-sm">
+                    Gerar
+                </p>
+            </a>
+
+            <a
+                href="{{ route('validate.cpf') }}"
+                class="bg-p-light dark:bg-p-dark rounded-lg p-4 text-white hover:bg-p-light-light dark:hover:bg-p-dark-light"
+                aria-label="Validador de CPF"
+            >
+                <h2 class="text-xl font-semibold mb-2">
+                    Validador de CPF
+                </h2>
+                <p class="mb-2">
+                    Valide um ou mais CPFs.
+                </p>
+                <p class="text-sm">
+                    Validar
+                </p>
+            </a>
+
+            <a
+                href="{{ route('edit.text') }}"
+                class="bg-p-light dark:bg-p-dark rounded-lg p-4 text-white hover:bg-p-light-light dark:hover:bg-p-dark-light"
+                aria-label="Editor de texto"
+            >
+                <h2 class="text-xl font-semibold mb-2">
+                    Editor de texto
+                </h2>
+                <p class="mb-2">
+                    Edite seu texto como quiser.
+                </p>
+                <p class="text-sm">
+                    Editar
+                </p>
+            </a>
+
+            <a
+                href="{{ route('generate.cnpj') }}"
+                class="bg-p-light dark:bg-p-dark rounded-lg p-4 text-white hover:bg-p-light-light dark:hover:bg-p-dark-light"
+                aria-label="Gerador de CNPJ"
+            >
+                <h2 class="text-xl font-semibold mb-2">
+                    Gerador de CNPJ
+                </h2>
+                <p class="mb-2">
+                    Gere um ou mais CNPJs válidos.
+                </p>
+                <p class="text-sm">
+                    Gerar
+                </p>
+            </a>
+
+            <a
+                href="{{ route('validate.cnpj') }}"
+                class="bg-p-light dark:bg-p-dark rounded-lg p-4 text-white hover:bg-p-light-light dark:hover:bg-p-dark-light"
+                aria-label="Validador de CNPJ"
+            >
+                <h2 class="text-xl font-semibold mb-2">
+                    Validador de CNPJ
+                </h2>
+                <p class="mb-2">
+                    Valide um ou mais CNPJs.
+                </p>
+                <p class="text-sm">
+                    Validar
+                </p>
+            </a>
+
+            <a
+                href="{{ route('convert.numbers') }}"
+                class="bg-p-light dark:bg-p-dark rounded-lg p-4 text-white hover:bg-p-light-light dark:hover:bg-p-dark-light"
+                aria-label="Conversor de números"
+            >
+                <h2 class="text-xl font-semibold mb-2">
+                    Conversor de números
+                </h2>
+                <p class="mb-2">
+                    Converta números para binário, octal, hexadecimal e romano.
+                </p>
+                <p class="text-sm">
+                    Converter
+                </p>
+            </a>
+        </div>
+    </main>
 @endsection
