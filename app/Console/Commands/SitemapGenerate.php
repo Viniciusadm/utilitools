3fc19b8dd11789357b60d3d785c3f38eb1d52e76
viniciusam <viniciusam.22@gmail.com>
2023-09-16 21:40:56 -0300

    Gerador de sitemap

---
+++ app/Console/Commands/SitemapGenerate.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SitemapGenerate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:sitemap-generate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Gerar sitemap.xml';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $init = '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
+        $init .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL;
+
+        $end = '</urlset>' . PHP_EOL;
+
+        $xml = $init;
+
+        $xml .= $this->addRoute('home');
+        $xml .= $this->addRoute('generate.cpf');
+        $xml .= $this->addRoute('generate.cnpj');
+        $xml .= $this->addRoute('validate.cpf');
+        $xml .= $this->addRoute('validate.cnpj');
+        $xml .= $this->addRoute('edit.text');
+        $xml .= $this->addRoute('convert.numbers');
+
+        $xml .= $end;
+
+        $file = fopen(public_path('sitemap.xml'), 'w');
+
+        fwrite($file, $xml);
+    }
+
+    private function addRoute(string $route): string
+    {
+        $xml = '<url>' . PHP_EOL;
+        $xml .= '<loc>' . route($route) . '</loc>' . PHP_EOL;
+        $xml .= '</url>' . PHP_EOL;
+
+        return $xml;
+    }
+}
