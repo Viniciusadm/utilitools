390ecce92868dbba331c4983b46411c14f736160
viniciusam <viniciusam.22@gmail.com>
2023-09-17 22:40:49 -0300

    Mudança na descrição da Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,12 @@
 
 @section('meta')
     <meta name="title" content="Ferramentas úteis online">
-    <meta name="description" content="Ferramentas úteis online. Gerador de CPF, validador de CPF, editor de texto">
+    <meta name="description" content="Ferramentas úteis online para testes, desenvolvimento e estudos. Geradores de CPF, CNPJ, números, editores de texto entre outras para agilizar a vida de programadores.">
     <meta name="keywords"
           content="ferramentas úteis, ferramentas úteis online, ferramentas online, ferramentas, gerador de cpf, gerador de cpf online, gerador de cpf válido, gerador de cpf aleatório, gerador de cpf com pontuação, gerador de cpf com estado, gerador de cpf com uf, gerador de cpf com unidade federativa, gerador de muitos cpfs, gerador de cpf em massa, gerador de cpf em lote, gerador de multiplos cpfs, validar cpf, validar cpf online, editor de texto, inverter texto, maiúsculas, minúsculas, capitalizar, alternar caixa, embaralhar, remover duplicados">
     <meta property="og:title" content="Ferramentas úteis online">
     <meta property="og:description"
-          content="Ferramentas úteis online. Gerador de CPF, validador de CPF, editor de texto">
+          content="Ferramentas úteis online para testes, desenvolvimento e estudos. Geradores de CPF, CNPJ, números, editores de texto entre outras para agilizar a vida de programadores.">
     <meta property="og:url" content="{{ route('home') }}">
 @endsection
 
