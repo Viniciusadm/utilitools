cd9651a06efce4f6b736ebd96dfe99fb1abe3f22
viniciusam <viniciusam.22@gmail.com>
2023-09-19 18:38:08 -0300

    Menu dinâmico

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Tool;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $tools = Tool::query()
+            ->orderBy('type')
+            ->get();
+
+        View::share([
+            'tools' => $tools,
+        ]);
     }
 }
--- resources/views/includes/aside.blade.php
+++ resources/views/includes/aside.blade.php
@@ -5,24 +5,13 @@
 >
     <div class="h-full overflow-y-auto p-4 bg-a-light dark:bg-a-dark rounded-lg">
         <ul class="space-y-2 font-medium">
-            @component('components.link-menu', ['name' => 'site.home', 'label' => 'Página inicial'])
+            @component('components.link-menu', ['name' => 'home', 'label' => 'Página inicial'])
             @endcomponent
-            @component('components.link-menu', ['name' => 'site.generate.cpf', 'label' => 'Gerador de CPF'])
-            @endcomponent
-            @component('components.link-menu', ['name' => 'site.validate.cpf', 'label' => 'Validador de CPF'])
-            @endcomponent
-            @component('components.link-menu', ['name' => 'site.generate.cnpj', 'label' => 'Gerador de CNPJ'])
-            @endcomponent
-            @component('components.link-menu', ['name' => 'site.validate.cnpj', 'label' => 'Validador de CNPJ'])
-            @endcomponent
-            @component('components.link-menu', ['name' => 'site.edit.text', 'label' => 'Editor de texto'])
-            @endcomponent
-            @component('components.link-menu', ['name' => 'site.generate.numbers', 'label' => 'Gerador de números'])
-            @endcomponent
-            @component('components.link-menu', ['name' => 'site.convert.numbers', 'label' => 'Conversor de números'])
-            @endcomponent
-            @component('components.link-menu', ['name' => 'site.convert.temperatures', 'label' => 'Conversor de temperaturas'])
-            @endcomponent
+
+            @foreach($tools as $tool)
+                @component('components.link-menu', ['name' => $tool->type . '.' . $tool->category, 'label' => $tool->name])
+                @endcomponent
+            @endforeach
         </ul>
     </div>
 </aside>
