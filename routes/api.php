acf96f03d2662327a6e631ac51f1a577a671bd99
viniciusam <viniciusam.22@gmail.com>
2023-09-21 01:21:56 -0300

    Rota de gerar nome

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GenerateController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,3 +13,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::name('api.')->group(function () {
+    Route::prefix('generate')->name('generator.')->group(function () {
+        Route::get('/name', [GenerateController::class, 'name'])->name('name');
+    });
+});
